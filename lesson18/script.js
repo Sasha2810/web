cfef7859440211fd514b7975bc694497b1a2fefe
https://Diana-Sereda@github.com/Diana-Sereda/practice-git.git <balovnev.artem@yandex.ru>
2023-01-24 21:25:33 +0700

    17 урок

---
+++ lesson18/script.js
@@ -0,0 +1,41 @@
+// let message;
+// message = 'Hello';
+
+// alert(message);
+// console.log(message);
+
+// let type1;
+// type1 = 9;
+// // console.log(`hello ** ${type1}`);
+// console.log(` ${type1**1/3}`);
+// // Оюратные ковычки используются для вывода текста и перменных
+// // + так "1" и 2 = "2"
+// // - так "1" и 2 = -1
+
+// // prompt - приображения переменных на экране
+// // let a = prompt("П", 1);
+// // let b = prompt("П", 2);
+
+// // + перед переменной означает бинарный плюс (перевод в числа)
+// alert(+a + +b);
+
+
+
+// let ves1 = prompt("Рост быстро");
+// let ves2  = prompt("вес быстро");
+// let rost1 = prompt("Рост быстро");
+// rost1 = rost1 / 100;
+// let rost2 = prompt("Рост быстро");
+// rost2 = rost2 / 100;
+// let e1, e2 = 0;
+// e1 = ves1 / rost1**2;
+// e1 = ves2 / rost2**2;
+// alert(e1 > e2 );
+
+
+
+const name = "Иван";
+const j = "учитель";
+const f = "2014";
+const t = "2023";
+console.log("Прошу внимания, это ваш новый", j, ",", name, "Его стаж работы", (t-f), "лет");
